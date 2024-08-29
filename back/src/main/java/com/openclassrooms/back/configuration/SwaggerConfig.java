9468ebbe4e0cbd283dcd9abb3e2357701023c443
guillaumebeysson <guillaume.beysson@gmail.com>
2024-08-29 09:50:34 +0200

    add configuration annotation in SwaggerConfig

--- back/src/main/java/com/openclassrooms/back/configuration/SwaggerConfig.java
+++ back/src/main/java/com/openclassrooms/back/configuration/SwaggerConfig.java
@@ -9,7 +9,9 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class SwaggerConfig {
 
     // Swagger link http://localhost:8080/swagger-ui/index.html
